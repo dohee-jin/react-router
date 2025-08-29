ec6ddeb189a014ff2bb256dcf9430f515350861b
dohee <doheeeq@gmail.com>
2025-08-29 10:36:17 +0900

    react-router: 에러 페이지 처리

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
 import styles from "./IndexPage.module.scss";
 
 const BlogPage = () => {
+
+    // 에러 페이지 테스트를 위한 강제 에러 발생
+    /*useEffect(() => {
+        throw new Error('알 수 없는 에러가 발생했습니다.');
+    }, [])*/
+
     return (
         <>
             <h1 className={styles.title}>
