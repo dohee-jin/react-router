2766549ed0f9fb5a774a377debb982c17b308738
dohee <doheeeq@gmail.com>
2025-08-29 14:44:47 +0900

    react-router: 사이드바에 필터링 추가

--- src/components/BlogSideBar.jsx
+++ src/components/BlogSideBar.jsx
@@ -1,28 +1,66 @@
-import { NavLink } from 'react-router-dom';
 import styles from './BlogSideBar.module.scss';
-import { categories } from '../dummy-data/dummy-post.js';
+import { categories, posts } from '../dummy-data/dummy-post.js';
+import {NavLink, useNavigate, useSearchParams} from 'react-router-dom';
 
 const BlogSideBar = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    // 새로고침 없이 리다이렉션을 지원하는 훅
+    const navigate = useNavigate();
+
+    const handleCategoryClick = (category) => {
+        // 사이드바에 있는 카테고리를 누르면 목록으로 이동하면서 카테고리를 보여줘야 함.
+        // location.href = '/blog';
+        navigate(`/blog`);
+        setSearchParams(prev => {
+            prev.set('category', category);
+            return prev;
+        });
+    };
+
+    // 게시물별 카테고리 수 카운팅하는 함수
+    const getCategoryCount = (category) => {
+        if(category === 'all') {
+            return posts.length;
+        }
+        return posts.filter(post => post.category === category).length;
+    }
+
+    // 최근 글 3개를 가져오기
+    const recentPosts = [...posts]
+        .sort((a, b) => new Date(b.date) - new Date(a.date))
+        .slice(0, 3);
 
     return (
         <aside className={styles.sidebar}>
             <h2>카테고리</h2>
             <ul className={styles.categoryList}>
-                {
-                    categories.map((category) => (
+                {categories.map((category) => (
                     <li key={category.id}>
-                        <button className={`${styles.categoryButton}`}>
+                        <button
+                            className={`${styles.categoryButton} ${(searchParams.get('category') || 'all') === category.id ? styles.active : ''}`}
+                            onClick={(e) => handleCategoryClick(category.id)}
+                        >
                             {category.name}
-                            <span className={styles.count}>2</span>
+                            <span className={styles.count}>{getCategoryCount(category.id)}</span>
                         </button>
                     </li>
-                ))
-                }
+                ))}
             </ul>
 
             <div className={styles.recentPosts}>
                 <h2>최근 글</h2>
-                <ul></ul>
+                <ul>
+                    {recentPosts.map(post =>
+                        <li key={post.id}>
+                            <NavLink to={`/blog/${post.id}`}>
+                                {post.title}
+                                <span className={styles.recentPostDate}>{post.date}</span>
+                            </NavLink>
+                        </li>
+                    )}
+                </ul>
             </div>
 
             <div className={styles.tags}>
